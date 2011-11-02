f17e4a0fd6e35bafef983fde3c9f632158e80ac8
Andrey Kuznecov <andreykyz@gmail.com>
2011-11-02 14:20:25 +0300

    Rewriting project to java

---
+++ src/ru/devhead/goatgame/logic/Card.java
@@ -0,0 +1,5 @@
+package ru.devhead.goatgame.logic;
+
+public class Card {
+
+}
---
+++ src/ru/devhead/goatgame/logic/CardsNames.java
@@ -0,0 +1,68 @@
+package ru.devhead.goatgame.logic;
+
+public class CardsNames {
+
+	// card's suit
+	static final int CROSSES = 3;
+	static final int PINOCHELE = 2;
+	static final int HEARTS = 1;
+	static final int DIAMONDS = 0;
+
+	// Значение карты
+	static final int SIX = 0;
+	static final int SEVEN = 4;
+	static final int EIGHT = 8;
+	static final int NINE = 12;
+	static final int TEN = 16;
+	static final int JACK = 20;
+	static final int QUEEN = 24;
+	static final int KING = 28;
+	static final int ACE = 32;
+
+	// Полное значение карты
+	// 6th
+	static final int SIX_CROSSES = SIX + CROSSES;
+	static final int SIX_PINOCHELE = SIX + PINOCHELE;
+	static final int SIX_HEARTS = SIX + HEARTS;
+	static final int SIX_DIAMONDS = SIX + DIAMONDS;
+	// 7th
+	static final int SEVEN_CROSSES = SEVEN + CROSSES;
+	static final int SEVEN_PINOCHELE = SEVEN + PINOCHELE;
+	static final int SEVEN_HEARTS = SEVEN + HEARTS;
+	static final int SEVEN_DIAMONDS = SEVEN + DIAMONDS;
+	// 8th
+	static final int EIGHT_CROSSES = EIGHT + CROSSES;
+	static final int EIGHT_PINOCHELE = EIGHT + PINOCHELE;
+	static final int EIGHT_HEARTS = EIGHT + HEARTS;
+	static final int EIGHT_DIAMONDS = EIGHT + DIAMONDS;
+	// 9th
+	static final int NINE_CROSSES = NINE + CROSSES;
+	static final int NINE_PINOCHELE = NINE + PINOCHELE;
+	static final int NINE_HEARTS = NINE + HEARTS;
+	static final int NINE_DIAMONDS = NINE + DIAMONDS;
+	// Десятки
+	static final int TEN_CROSSES = TEN + CROSSES;
+	static final int TEN_PINOCHELE = TEN + PINOCHELE;
+	static final int TEN_HEARTS = TEN + HEARTS;
+	static final int TEN_DIAMONDS = TEN + DIAMONDS;
+	// Jacks
+	static final int JACK_CROSSES = JACK + CROSSES;
+	static final int JACK_PINOCHELE = JACK + PINOCHELE;
+	static final int JACK_HEARTS = JACK + HEARTS;
+	static final int JACK_DIAMONDS = JACK + DIAMONDS;
+	// Queens
+	static final int QUEEN_CROSSES = QUEEN + CROSSES;
+	static final int QUEEN_PINOCHELE = QUEEN + PINOCHELE;
+	static final int QUEEN_HEARTS = QUEEN + HEARTS;
+	static final int QUEEN_DIAMONDS = QUEEN + DIAMONDS;
+	// Kings
+	static final int KING_CROSSES = KING + CROSSES;
+	static final int KING_PINOCHELE = KING + PINOCHELE;
+	static final int KING_HEARTS = KING + HEARTS;
+	static final int KING_DIAMONDS = KING + DIAMONDS;
+	// Aces;
+	static final int ACE_CROSSES = ACE + CROSSES;
+	static final int ACE_PINOCHELE = ACE + PINOCHELE;
+	static final int ACE_HEARTS = ACE + HEARTS;
+	static final int ACE_DIAMONDS = ACE + DIAMONDS;
+}
