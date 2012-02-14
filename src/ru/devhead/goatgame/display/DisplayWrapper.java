5df5e61b86f241b89a5eec37de8dbe2e117f946d
Kyznecov Andrey <andreykyz@gmail.com>
2012-02-14 22:14:34 +0400

    add fixOffsets()

--- src/ru/devhead/goatgame/display/DisplayWrapper.java
+++ src/ru/devhead/goatgame/display/DisplayWrapper.java
@@ -20,8 +20,8 @@ import ru.devhead.goatgame.logic.CardsNames;
 /**
  * @author Andrey Kuznetsov
  */
-public class DisplayWrapper extends JComponent implements ComponentListener, MouseListener,
-		MouseMotionListener, Display {
+public class DisplayWrapper extends JComponent implements ComponentListener,
+		MouseListener, MouseMotionListener, Display {
 
 	// constants
 	private static final long serialVersionUID = 1L;
@@ -29,13 +29,13 @@ public class DisplayWrapper extends JComponent implements ComponentListener, Mou
 	private static final Color TEXT_COLOUR = Color.BLACK;
 	private static final int TABLE_WIDTH = 800; // Pixels.
 	private static final int TABLE_HEIGHT = 550; // Pixels.
-	
+
 	// fields
 	private Point trumpSuitPoint;
 	private ImageIcon trumpSuitImg;
 	private Point textPoint;
 
-	private String textLine = "New game";
+	private String textLine = "Kozel card game";
 
 	private CardBatch leftBatch;
 	private CardBatch rightBatch;
@@ -47,7 +47,7 @@ public class DisplayWrapper extends JComponent implements ComponentListener, Mou
 
 	public DisplayWrapper() {
 		setPreferredSize(new Dimension(TABLE_WIDTH, TABLE_HEIGHT));
-		
+
 		leftBatch = new CardBatch();
 		rightBatch = new CardBatch();
 		topBatch = new CardBatch();
@@ -55,18 +55,17 @@ public class DisplayWrapper extends JComponent implements ComponentListener, Mou
 
 		turnedCards = new CardWrapper[4];
 		turnedCardsIndex = 0;
-		
+
 		trumpSuitImg = new ImageIcon();
-		trumpSuitPoint = new Point(0,0);
-		
+		trumpSuitPoint = new Point(0, 0);
+
 		textPoint = new Point(5, TABLE_HEIGHT - 10);
-		
-		// ... Add listeners.
+
+		// Add listeners ...
 		this.addComponentListener(this);
 		this.addMouseListener(this);
 		this.addMouseMotionListener(this);
-	}		
-	
+	}
 
 	@Override
 	public void paintComponent(Graphics g) {
@@ -75,22 +74,20 @@ public class DisplayWrapper extends JComponent implements ComponentListener, Mou
 		int height = getHeight();
 		g.setColor(BACKGROUND_COLOUR);
 		g.fillRect(0, 0, width, height);
-		
+
 		// Draw "say string" on the bottom
 		g.setColor(TEXT_COLOUR);
 		g.drawString(textLine, textPoint.x, textPoint.y);
 
 		// Painting trump suit image
 		trumpSuitImg.paintIcon(this, g, trumpSuitPoint.x, trumpSuitPoint.y);
-		
+
 		// Painting turned cards on the center
 		for (int i = 0; i < 4; i++) {
 			if (turnedCards[i] != null) {
 				turnedCards[i].draw(g, this);
 			}
 		}
-		
-		
 
 	}
 
@@ -104,31 +101,8 @@ public class DisplayWrapper extends JComponent implements ComponentListener, Mou
 				turnedCards[3] = null;
 			}
 			turnedCards[turnedCardsIndex++] = new CardWrapper(card);
-			turnedCards[turnedCardsIndex-1].setVisible(true);
-			// Hardcoded offset - 5 pixels
-			switch (turnedCardsIndex - 1) {
-			case 0:
-				turnedCards[0]
-						.moveTo(this.getWidth() / 2
-								- (turnedCards[0].getWidth() + 5),
-								this.getHeight() / 2
-										- (turnedCards[0].getHeight() + 5));
-				break;
-			case 1:
-				turnedCards[1].moveTo(this.getWidth() / 2 + 5, this.getHeight()
-						/ 2 - (turnedCards[1].getHeight() + 5));
-				break;
-			case 2:
-				turnedCards[2].moveTo(this.getWidth() / 2 + 5,
-						this.getHeight() / 2 + 5);
-				break;
-			case 3:
-				turnedCards[3].moveTo(
-						this.getWidth() / 2 - (turnedCards[3].getWidth() + 5),
-						this.getHeight() / 2 + 5);
-				break;
-
-			}
+			turnedCards[turnedCardsIndex - 1].setVisible(true);
+			fixOffsets();
 		} else {
 			turnedCardsIndex = 0;
 			turnedCards[0] = null;
@@ -245,28 +219,60 @@ public class DisplayWrapper extends JComponent implements ComponentListener, Mou
 	@Override
 	public void componentHidden(ComponentEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void componentMoved(ComponentEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void componentResized(ComponentEvent arg0) {
-		// Hardcoded right top position and offset is dublesize of image
-		trumpSuitPoint = new Point(this.getWidth()
-				- trumpSuitImg.getIconWidth() * 2,
-				trumpSuitImg.getIconHeight() * 2);
-		textPoint = new Point(5, this.getHeight() - 10);
-		this.repaint();		
+		fixOffsets();
+		this.repaint();
 	}
 
 	@Override
 	public void componentShown(ComponentEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	/**
+	 * Procedure for fix component's position
+	 */
+	private void fixOffsets() {
+		// Hardcoded right top position for current suit
+		trumpSuitPoint = new Point(this.getWidth()
+				- trumpSuitImg.getIconWidth() * 2,
+				trumpSuitImg.getIconHeight() * 2);
+
+		// Hardcoded position for Text line at down
+		textPoint = new Point(5, this.getHeight() - 10);
+
+		// Hardcoded offset for turned cards - 5 pixels
+		switch (turnedCardsIndex - 1) {
+		case 0:
+			turnedCards[0].moveTo(
+					this.getWidth() / 2 - (turnedCards[0].getWidth() + 5),
+					this.getHeight() / 2 - (turnedCards[0].getHeight() + 5));
+			break;
+		case 1:
+			turnedCards[1].moveTo(this.getWidth() / 2 + 5, this.getHeight() / 2
+					- (turnedCards[1].getHeight() + 5));
+			break;
+		case 2:
+			turnedCards[2].moveTo(this.getWidth() / 2 + 5,
+					this.getHeight() / 2 + 5);
+			break;
+		case 3:
+			turnedCards[3].moveTo(
+					this.getWidth() / 2 - (turnedCards[3].getWidth() + 5),
+					this.getHeight() / 2 + 5);
+			break;
+		}
+
 	}
 }
