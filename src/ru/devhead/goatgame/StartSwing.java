7a99d61e51f88c6c9cdfa9e81302882bf78aa20a
Kyznecov Andrey <andreykyz@gmail.com>
2012-02-26 12:30:58 +0400

    added starter

---
+++ src/ru/devhead/goatgame/StartSwing.java
@@ -0,0 +1,15 @@
+package ru.devhead.goatgame;
+
+import ru.devhead.goatgame.display.WindowSwing;
+
+public class StartSwing {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new WindowSwing();
+
+	}
+
+}
