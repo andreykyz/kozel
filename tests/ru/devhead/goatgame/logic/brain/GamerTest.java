f3957d69f5b2c6ec6c5cad99d5b599401ba407f0
Kyznecov Andrey <andreykyz@gmail.com>
2012-04-01 13:39:00 +0400

    GamerTest.java - full

--- tests/ru/devhead/goatgame/logic/brain/GamerTest.java
+++ tests/ru/devhead/goatgame/logic/brain/GamerTest.java
@@ -65,9 +65,13 @@ public class GamerTest {
 		assertFalse(Gamer.isSuperTrump(card), "isNotSuperTrump " + card.getName());
 	}
 
-	@Test
-	public void suitTest() {
-		//Доделать
-		throw new RuntimeException("Test not implemented");
+	@Test(dataProvider = "notSuperTrumps")
+	public void suitTest(Card card) {
+		assertTrue(Gamer.suitTest(card, card.getSuitId()));
+	}
+
+	@Test(dataProvider = "noTrumpAndTrumpSuitPairs")
+	public void suitTestFalse(Card card, int suit) {
+		assertFalse(Gamer.suitTest(card, suit));
 	}
 }
--- tests/ru/devhead/goatgame/logic/verification/CardGroups.java
+++ tests/ru/devhead/goatgame/logic/verification/CardGroups.java
@@ -20,6 +20,8 @@ public class CardGroups {
 	static public Card[] notSuperTrumps;
 	static public Object[][] trumpAndTrumpSuitPairs;
 	static public Object[][] noTrumpAndTrumpSuitPairs;
+	static public Object[][] CardAndSuitPairs;
+	static public Object[][] CardAndNoSuitPairs;
 
 	static int[] SuperTrumpId = { CardsNames.SIX_CROSSES,
 			CardsNames.JACK_DIAMONDS, CardsNames.JACK_HEARTS,
@@ -114,5 +116,8 @@ public class CardGroups {
 			}
 		}
 		noTrumpAndTrumpSuitPairs = noTrumpAndTrumpSuitPairsList.toArray(new Object[noTrumpAndTrumpSuitPairsList.size()][2]);
+		
+		List<Object[]> CardAndSuitPairsList = new ArrayList<Object[]>();
+		
 	}
 }
